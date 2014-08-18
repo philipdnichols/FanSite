5586a8fdae62879cd53af4dd984e0c7ee8ec2e70
Phil Nichols <philipdnichols@gmail.com>
2014-08-18 13:51:46 -0400

    Just a bit of preparation code.

--- FanSite/FanSiteFeedTableViewController.m
+++ FanSite/FanSiteFeedTableViewController.m
@@ -7,11 +7,42 @@
 //
 
 #import "FanSiteFeedTableViewController.h"
+#import "ArrayDataSource.h"
 
 @interface FanSiteFeedTableViewController ()
+
+@property (strong, nonatomic) ArrayDataSource *feedArrayDataSource;
 
 @end
 
 @implementation FanSiteFeedTableViewController
+
+#pragma mark - Properties
+
+- (void)setFeedArrayDataSource:(ArrayDataSource *)feedArrayDataSource
+{
+    _feedArrayDataSource = feedArrayDataSource;
+    
+    self.tableView.dataSource = _feedArrayDataSource;
+    [self.tableView reloadData];
+}
+
+#pragma mark - Lifecycle
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    [self refreshFeed];
+}
+
+#pragma mark - IBActions
+
+- (IBAction)refreshFeed
+{
+    [self.refreshControl beginRefreshing];
+    
+    [self.refreshControl endRefreshing];
+}
 
 @end
--- FanSite/FanSiteTweetsTableViewController.m
+++ FanSite/FanSiteTweetsTableViewController.m
@@ -7,11 +7,42 @@
 //
 
 #import "FanSiteTweetsTableViewController.h"
+#import "ArrayDataSource.h"
 
 @interface FanSiteTweetsTableViewController ()
+
+@property (strong, nonatomic) ArrayDataSource *tweetsArrayDataSource;
 
 @end
 
 @implementation FanSiteTweetsTableViewController
+
+#pragma mark - Properties
+
+- (void)setTweetsArrayDataSource:(ArrayDataSource *)tweetsArrayDataSource
+{
+    _tweetsArrayDataSource = tweetsArrayDataSource;
+    
+    self.tableView.dataSource = _tweetsArrayDataSource;
+    [self.tableView reloadData];
+}
+
+#pragma mark - Lifecycle
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    [self refreshTweets];
+}
+
+#pragma mark - IBActions
+
+- (IBAction)refreshTweets
+{
+    [self.refreshControl beginRefreshing];
+    
+    [self.refreshControl endRefreshing];
+}
 
 @end
