686c19ad7a22dfed9617ab1c2f363cafb1cb2ae3
Phil Nichols <philipdnichols@gmail.com>
2014-08-18 12:58:52 -0400

    initial commit

---
+++ FanSite/FanSiteAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  FanSiteAppDelegate.h
+//  FanSite
+//
+//  Created by Philip Nichols on 8/18/14.
+//  Copyright (c) 2014 Phil Nichols. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FanSiteAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FanSite/FanSiteViewController.h
@@ -0,0 +1,13 @@
+//
+//  FanSiteViewController.h
+//  FanSite
+//
+//  Created by Philip Nichols on 8/18/14.
+//  Copyright (c) 2014 Phil Nichols. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FanSiteViewController : UIViewController
+
+@end
